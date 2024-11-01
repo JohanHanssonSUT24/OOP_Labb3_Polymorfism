18155325943be2906f80c91d0cec3a7fcf6a56a2
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-01 13:04:25 +0100

    Done with main addings

--- OOP_Labb3_Polymorfism/Program.cs
+++ OOP_Labb3_Polymorfism/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Geometry> geometry = new List<Geometry>();
+
+            Geometry geoR = new Rectangle();
+
+            Geometry geoC = new Circle();
+
+            Geometry geoS = new Square();
+
+            geometry.Add(geoR);
+            geometry.Add(geoC);
+            geometry.Add(geoS);
+
+            foreach (Geometry g in geometry)
+            {
+                Console.WriteLine(g.Area());
+            }
         }
     }
 }
