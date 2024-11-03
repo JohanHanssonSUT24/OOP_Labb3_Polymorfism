3927a334a21f1a49d762b6ae43d014c4eaa59ffa
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-03 17:27:38 +0100

    Done with comments

--- OOP_Labb3_Polymorfism/Geometry.cs
+++ OOP_Labb3_Polymorfism/Geometry.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP_Labb3_Polymorfism
+﻿namespace OOP_Labb3_Polymorfism
 {
+    //Created abstract class due to security. I dont want anyone to create an instance of this class but of the subclasses.
     public abstract class Geometry
     {
+        //Make abstract class so user must declare its own implementations within the subclasses.
         public abstract double Area();
     }
 }
